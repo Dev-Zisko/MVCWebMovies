3fbb4924c43ae4d49824f3da77abf19eecbad4a5
Dev-Zisko <dev.oliverfrancisco@gmail.com>
2021-06-17 19:48:45 -0400

    Subida de Archivos

---
+++ Data/WebtestMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebtestMVC.Models;
+
+namespace WebtestMVC.Data
+{
+    public class WebtestMVCContext : DbContext
+    {
+        public WebtestMVCContext (DbContextOptions<WebtestMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
